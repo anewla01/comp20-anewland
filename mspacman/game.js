2931e18ce12019f1b5e94137db08d4f44b3c22ca
anewla01 <andre.newland@tufts.edu>
2015-03-01 13:17:38 -0500

    correction: only blue game board is now seen

--- mspacman/game.js
+++ mspacman/game.js
@@ -4,7 +4,8 @@ function init(){
 	img.src = "pacman10-hp-sprite.png";
 	img.addEventListener("load", function() {
 		var ctx = document.getElementById("game_canvas").getContext("2d");
-		ctx.drawImage(img, -320, 0);
+		// ctx.drawImage(img, -320, -140);
+		ctx.drawImage(img, 320, 0, 465, 140, 0,0, 465, 140);
 		ctx.drawImage(img, 80, 20, 19, 20, 10, 5, 20, 19);
 	}, false);
 

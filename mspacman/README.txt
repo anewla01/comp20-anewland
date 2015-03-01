603d3b63ab0e863122e71ac4843cb830c70b4e2c
anewla01 <andre.newland@tufts.edu>
2015-03-01 13:11:28 -0500

    finished script

---
+++ mspacman/README.txt
@@ -0,0 +1,12 @@
+/* 
+* Andre C. Newland
+* anewla01
+* Assignment: Lab 5
+*/
+
+
+
+    Correct Implementations: Correctly implemented a javascript to slice an imave and draw onto a canvas
+    Incorrect Implementatin: N/A
+    Completion Time: 1.25 hour
+    Collaborators: None
--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init(){
+// 	console.log("up and running");
+	var img = new Image();
+	img.src = "pacman10-hp-sprite.png";
+	img.addEventListener("load", function() {
+		var ctx = document.getElementById("game_canvas").getContext("2d");
+		ctx.drawImage(img, -320, 0);
+		ctx.drawImage(img, 80, 20, 19, 20, 10, 5, 20, 19);
+	}, false);
+
+}

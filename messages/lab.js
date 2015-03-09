7e8831ff53f85ee687e2abb05d9861b4b481873b
anewla01 <andre.newland@tufts.edu>
2015-03-08 21:39:00 -0400

    slow progress

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
 // Your JavaScript code here...
+
+ function parse(){
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+
+	//function called three times 
+	console.log("hello");
+	request.onreadystatechange = parse_data; //parseData is function 
+ 	request.send();
+ }
+
+
+function parse_data(){
+	if (request.readyState == 4) {
+		messagesDiv = document.getElementById("messages");
+		converted JSON.parse(request.responseText);
+		// for(int i = 0; i < converted.length; i++) {
+		// 	// messageDiv.innerHTML += "<p>" + converted[i]["content"];
+		// }
+	}
+}
+
+
